6a5e0a19195eb1fc419091baa6dc0d2feebb846f
Daniel Schulman <daniel@textmylink.com>
2025-07-13 18:09:37 -0700

    Add passthrough copy for config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 module.exports = function(eleventyConfig) {
+  // Tell Eleventy to copy config.yml to the output folder
+  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+
   return {
     dir: {
       input: "src",
+      includes: "_includes",
       output: "_site"
     }
   };
